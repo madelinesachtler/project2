ceb3693bbe05549377f857bf54517669ec63a3f8
madelinesachtler <73665864+madelinesachtler@users.noreply.github.com>
2021-03-20 17:59:26 -0700

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,15 @@
+drop table combined_data;
+
+	CREATE TABLE combined_data ( 
+	Country_ID INT,
+	Country VARCHAR PRIMARY KEY,
+	Region VARCHAR,
+	Population INT,
+	Freedom_Ranking SERIAL,
+	GDP_per_Capita INT,
+	Freedom_to_make_life_choices INT	
+)
+---------------------------------------------------
+
+
+
